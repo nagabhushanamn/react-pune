30114f38fa6ded0ddc8fc4ca3fc9cec629983980
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2018-10-03 13:13:52 +0530

    types & scopes

---
+++ 1-JS/1.data-types.js
@@ -0,0 +1,232 @@
+
+
+/*
+
+
+    2 types
+    --------
+    1. simple / primitives ==> values
+
+        a.  undefined
+        b.  string
+        c.  number
+        d.  boolean
+        
+    2. complex / reference ==> objects
+
+
+*/
+
+
+//-----------------------------------------------------------------
+// 1. simple / primitives ==> values
+//-----------------------------------------------------------------
+// a. undefined
+
+var v;
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+// b. string
+var name = "Nag";
+var selection = 'abc';
+// var dynamicString = 'the trainer is ' + name;
+// or
+var dynamicString = `the trainer is ${name}`  // Es6  ==> string interpolation
+var multiLineString = `
+    <div>
+        <h1>heading</h1>
+    </div>
+`;
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+// 3. number
+
+var count = 12;
+var cost = 12.12;
+
+//-----------------------------------------------------------------
+
+// 4. boolean
+
+var found = true;
+
+
+// imp-note :
+
+/*
+    falsy values:
+    => false,0,"",null,undefined,NAN
+    ref : https://dorey.github.io/JavaScript-Equality-Table/
+*/
+
+
+
+//-----------------------------------------------------------------
+// 2. complex / reference ==> objects
+//-----------------------------------------------------------------
+
+
+/*
+    
+    how to create object in .js-language ?
+
+    syntax:
+
+    var ref=new Constructor();
+
+    by default , 
+    all .js-objects are 'extensible' & 'configurable'
+
+*/
+
+// ES5
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+
+// Person.prototype.sayName = function () {
+//     console.log('im ' + this.name);
+// }
+// Person.prototype.sayAge = function () {
+//     console.log('im ' + this.age + " old");
+// }
+
+// ES6 ==> ( New OO syntax with class )
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        console.log('Person::constructor');
+    }
+    sayName() {
+        console.log('im ' + this.name);
+    }
+    sayAge() {
+        console.log('im ' + this.age + " old");
+    }
+}
+
+//var p1 = new Person('Nag', 35);
+
+
+class Employee extends Person {
+    constructor(name, age, salary) {
+        super(name, age);
+        this.salary = salary;
+        console.log('Employee::constructor');
+    }
+    saySalary() {
+        console.log('i wont');
+    }
+    askForRaise() {
+        return this.salary * 0.02;
+    }
+}
+
+//var emp = new Employee('Nag', 35, 1000.00)
+
+
+class Boss extends Employee {
+    askForRaise() {
+        return this.salary * 0.2 + super.askForRaise();
+    }
+}
+
+var boss = new Boss('Nag', 35, 1000.00)
+
+
+class Abc {
+    static staMethod() {
+        //..
+    }
+}
+Abc.staVar = 12;
+
+
+
+
+//-----------------------------------------------------------------
+// Literal style objects
+//-----------------------------------------------------------------
+
+
+// 1. Object
+
+var config = new Object();
+config.url = "http://"
+config.httpMethod = "GET"
+config.onSuccess = function () {
+    console.log('im running on success');
+}
+
+// or
+
+var newConfig = {
+    url: 'http://',
+    httpMethod: 'GET',
+    onSuccess: function () {
+        //..
+    }
+};
+
+
+// 2. Array / List
+
+var arr = new Array();
+arr[0] = "poha";
+arr.push('poha');
+
+// or
+var newArr = ['item1', 'item2'];
+
+
+// 3. Regular Expression
+
+var re = new RegExp('\\d{10}');
+
+// or
+
+var newRe = /\d{10}/;
+
+
+// 4. Function
+
+var add = new Function("n1", "n2", "var result=n1+n2;return result;")
+// or
+function newAdd(n1, n2) {
+    var result = n1 + n2; return result
+}
+
+
+
+//------------------------------------------------------------
+
+
+// How to access obj's properties?
+
+/*
+
+    way-1 : '.' , if property dont have any '-' or 'space' else 
+    way-2 : '[]'
+
+*/
+
+var person = {
+    'name': 'Nag',
+    'home-address': 'chennai',
+    100: 'this is hundred'
+};
+
+person.name = "Nag N";
+person["home-address"] = "pune";
+person["100"] = '...'
+
+
+//------------------------------------------------------------
---
+++ 1-JS/2-scopes.js
@@ -0,0 +1,155 @@
+"use strict"
+
+
+/*
+
+    scope / execution-context
+    ---------------------------
+
+    
+    memory/stack-frame with given arg & local variables
+    to execute instructions in seq
+
+    phase-1: creation/push
+
+        ==> any variable declared with 'var' keyword,
+            always get hoisted with default value
+
+    phase-2: execution/pop
+
+        ==> instruction will execute in seq.
+
+    --------------------------------------------------------    
+
+    imp-note:
+
+    by default, every .js-runtime has 1 global-context/scope
+
+    every function invocation also creates new-scope which is child of
+    in which scope that function has declared/created
+
+
+*/
+
+
+//---------------------------------------------------------
+// console.log(v);
+// var v = 12;
+//---------------------------------------------------------
+
+
+// var v = 12;
+// function f1() {
+//     console.log(v);
+//     var v = 13;
+// }
+// f1(); // f1-scope   <-- global-scope
+
+//---------------------------------------------------------
+
+// Quiz
+
+// var v = 12;
+// function f1() {
+//     function f2() {
+//         console.log(v);
+//     }
+//     f2();  // f2-scope <---- f1-scope
+//     var v = 13;
+//     f2();  // f2-scope <---- f1-scope
+// }
+// f1(); // f1-scope <---- global-scope
+
+//---------------------------------------------------------
+
+
+// var v = 12;
+// var v = 13;
+
+//---------------------------------------------------------
+
+// var v = 100;
+// if (true) {
+//     var v = 200;
+// }
+// console.log(v);
+
+//---------------------------------------------------------
+
+/*
+
+    probblems with 'var' keyword
+
+    => always get hoist
+    => we can re-declare same variable within scope
+    => No block-scope to variables
+
+    soln : using 'let' & 'const' keywords with block-scope  ( in ES6 )
+
+*/
+
+
+
+//---------------------------------------------------------
+
+// console.log(v);
+// let v = 12;
+
+//---------------------------------------------------------
+
+// var v=12;
+// let v=13;
+
+//---------------------------------------------------------
+
+// var v = 100;
+// if (true) {
+//     let v = 200;
+// }
+// console.log(v);
+
+//---------------------------------------------------------
+
+
+const trainer = {
+    name: 'Nag'
+};
+
+// trainer = null;// Error
+trainer.name = "Nag N";
+
+
+// summary :
+
+// use 'let' for mutable reference
+// use 'const' for immutable reference
+// avoid 'var' keyword
+
+
+//---------------------------------------------------------
+
+let person = {
+    name: 'Nag',
+    age: 35
+};
+
+// Object.defineProperty(person, 'name', { writable: false })
+Object.freeze(person)
+
+// person.age = 0;
+
+//---------------------------------------------------------
+
+
+// Quiz
+
+
+function getFood() {
+    return{
+        name: "Biryani"
+    }
+}
+
+
+let food = getFood();
+console.log(food.name);
